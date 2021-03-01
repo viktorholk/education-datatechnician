f7ed93206e308638b7114b467095be25fb877c5a
Viktor Holk <viktorholk@gmail.com>
2021-03-01 10:17:25 +0100

    Calculator evaluate class

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 class Calculator:
-    pass
+    def evaluate(self, string):
+        try:
+            return eval(string)
+        except:
+            return 'Invalid input'
 
 
 if __name__ == '__main__':
     calculator = Calculator()
-    
+    print('H1 Calculator')
+    while True:
+        _input  = input('> ')
+        _output = calculator.evaluate(_input)
+        print(_output)
