786cd026cb312ba0ca7b45bda7a5338c6116fda0
Viktor Holk Gamskjær <viktorholk@gmail.com>
2021-03-16 08:25:24 +0100

    Added CleanCode Assignment

---
+++ H1/CleanCodeExersises/CleanCodeExersises/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanCodeExersises
+{
+    public class Employee
+    {
+        public int Age;
+        public int YearsEmployed;
+        public bool IsRetired;
+    }
+}
---
+++ H1/CleanCodeExersises/CleanCodeExersises/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * This project is a collection of exersises that will teach students how to write clean and DRY code.
+ * These exersises are stutable for beginer to experienced programmers.
+ */
+namespace CleanCodeExersises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        /*
+         * Magic Number exercise
+         */
+        public bool IsLegalDrikingAgeDirty(int age)
+        {
+            if (age > 21)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /*
+         * Solution to Magic Number exercise
+         * Use consts to describe the numbers you are using to your programs.
+         */
+        public bool IsLegalDrikingAgeClean()
+        {
+            throw new NotImplementedException();
+            //Write your solution here
+        }
+        /*
+         * Be positive exersise
+         */
+        public bool IsLoggedInDirty(bool isNotloggedIn)
+        {
+            if (!isNotloggedIn == true)
+            {
+                System.Console.WriteLine("Succesfully logged in.");
+                return true;
+            }
+            else
+            {
+                System.Console.WriteLine("Failed to logged in.");
+                return false;
+            }
+        }
+        /*
+         * Solution to Be positive exersise
+         */
+        public bool IsLoggedInClean()
+        {
+            throw new NotImplementedException();
+            //Write your solution here
+        }
+        /*
+         * Intermediate exercise
+         * What is the question answering?
+         */
+        public bool eligibleDirty(Employee employee)
+        {
+            if (employee.Age > 55
+                && employee.YearsEmployed > 10
+                && employee.IsRetired == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /*
+         * Solution to ohm Intermediate exercise
+         * An intermediate Variable says a lot more them a long expression
+         */
+        public bool eligibleClean()
+        {
+            throw new NotImplementedException();
+            //Write your solution here
+        }
+        
+        /*
+         * Ternary exersise
+         */
+        public double GetPriceDirty(bool isPreordered)
+        {
+            if (isPreordered == true)
+            {
+                return 200.00;
+            } else
+            {
+                return 350.00;
+            }
+        }
+        /*
+         * Solution to Ternary exersise
+         * Use a oneline Ternary.
+         */
+        public double GetPriceClean()
+        {
+            throw new NotImplementedException();
+            //Write your solution here
+        }
+        /*
+         * Strong type exersise - not done
+         */
+
+    }
+}
+
+
