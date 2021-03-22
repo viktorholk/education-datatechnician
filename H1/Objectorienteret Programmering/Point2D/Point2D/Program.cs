543e51f4de091e84c9c0e61d718bfd8236db46aa
Viktor Holk Gamskjær <viktorholk@gmail.com>
2021-03-22 12:48:23 +0100

    Override ToString method

--- H1/Objectorienteret Programmering/Point2D/Point2D/Program.cs
+++ H1/Objectorienteret Programmering/Point2D/Point2D/Program.cs
@@ -12,16 +12,15 @@ namespace Point2D
             this.x = _x;
             this.y = _y;
         }
-
-        public string GetPoints()
-        {
-            return $"( {this.x}, {this.y} )";
-        }
-
         public double Distance(Point2D p2)
         {
             
             return Math.Sqrt(Math.Pow(p2.x - this.x, 2) + Math.Pow(p2.y - this.y,2));
+        }
+
+        public override string ToString()
+        {
+            return $"( {this.x}, {this.y} )";
         }
     }
     class Program
@@ -30,7 +29,7 @@ namespace Point2D
         {
             Point2D point1 = new Point2D(5, 10);
             Point2D point2 = new Point2D(8, 12);
-            Console.Write($"The distance between {point1.GetPoints()} and {point2.GetPoints()}  = ");
+            Console.Write($"The distance between {point1.ToString()} and {point2.ToString()}  = ");
             Console.Write(point1.Distance(point2));
             Console.WriteLine();
         }
