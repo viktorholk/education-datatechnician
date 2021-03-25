eea225f2f7ddf5e3e1361b3fb7d3972f9c9c4124
Viktor Holk Gamskjær <viktorholk@gmail.com>
2021-03-25 10:38:46 +0100

    [A-Z] sort, overload for QuickSort

--- H1/Grundlaeggende Programmering/SortingAlgorithm/SortingAlgorithm/Program.cs
+++ H1/Grundlaeggende Programmering/SortingAlgorithm/SortingAlgorithm/Program.cs
@@ -18,7 +18,36 @@ namespace SortingAlgorithm
             }
         }
 
+        public void QuickSort(char[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+
+            }                
+        }
+
         private int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+
+            int i = low - 1;
+
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            Swap(arr, i + 1, high);
+            return (i + 1);
+        }
+
+        private int Partition(char[] arr, int low, int high)
         {
             int pivot = arr[high];
 
@@ -42,6 +71,13 @@ namespace SortingAlgorithm
             arr[i] = arr[j];
             arr[j] = temp;
         }
+
+        private void Swap(char[] arr, int i, int j)
+        {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
     }
 
 
@@ -57,8 +93,15 @@ namespace SortingAlgorithm
             PrintIntArray(iArr);
             sort.QuickSort(iArr, 0, n - 1);
             PrintIntArray(iArr);
+            // [A-Z] sort
+            string myString = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            char[] cArr = myString.ToCharArray();
+            int _n = cArr.Length;
+            Console.WriteLine(myString);
+            sort.QuickSort(cArr, 0, _n - 1);
+            Console.WriteLine(string.Join("", cArr));
 
-            
+
         }
 
         static void PrintIntArray(int[] arr)
