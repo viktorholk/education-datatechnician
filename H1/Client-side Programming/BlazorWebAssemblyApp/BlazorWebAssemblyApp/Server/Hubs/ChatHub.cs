f0bc03e68feffe128b45241ec051e4e52f2cfff5
Viktor Holk Gamskjær <viktorholk@gmail.com>
2021-04-14 11:02:09 +0200

    BlazerWebAssemblyApp INIT

---
+++ H1/Client-side Programming/BlazorWebAssemblyApp/BlazorWebAssemblyApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("ec0f065d-be7f-48d6-b90b-dbf8a2b3afba")]
+
+namespace BlazorWebAssemblySignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
