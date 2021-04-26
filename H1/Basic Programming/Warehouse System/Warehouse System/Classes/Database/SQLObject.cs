3a6e812fee1e08c9e0266c6a724202f587a3da5b
viktorholk <viktorholk@gmail.com>
2021-04-26 09:07:10 +0200

    Create SQLObject.cs

---
+++ H1/Basic Programming/Warehouse System/Warehouse System/Classes/Database/SQLObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse_System
+{
+    abstract class SQLObject
+    {
+        public int Id { get; set; }
+
+        public abstract void Save();
+        public abstract void Remove();
+    }
+}
