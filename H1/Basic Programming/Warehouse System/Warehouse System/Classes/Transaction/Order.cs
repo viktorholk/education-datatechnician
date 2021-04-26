1d9d2950b641da8ab780fd214e9c39525a1eab79
viktorholk <viktorholk@gmail.com>
2021-04-26 10:30:15 +0200

    Update Order.cs
    Order can now save and remove from db

--- H1/Basic Programming/Warehouse System/Warehouse System/Classes/Transaction/Order.cs
+++ H1/Basic Programming/Warehouse System/Warehouse System/Classes/Transaction/Order.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace Warehouse_System
 {
     class Order : Transaction
@@ -12,13 +12,36 @@ namespace Warehouse_System
         {
             this.Customer = customer;
         }
+        public Order(int id, Customer customer)
+        {
+            this.Id = id;
+            this.Customer = customer;
+            this.Saved = true;
+        }
         public override void Save()
         {
-
+            if (!Saved)
+            {
+                Database.Execute($@"
+                                    INSERT INTO orders
+                                    (customer_id, date)
+                                    VALUES ('{this.Customer.Id}', '{this.CreationDate}')");
+                Console.WriteLine($"Created order for customer {this.Customer.FirstName} {this.Customer.LastName}");
+            }
         }
         public override void Remove()
         {
-
+            if (Saved)
+            {
+                Database.Execute($@"
+                                    DELETE FROM orders
+                                    WHERE id = {this.Id}");
+                Console.WriteLine($"Removed order, {this.Id}");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{CreationDate}, {this.Customer.FirstName} {this.Customer.LastName}";
         }
     }
 }
