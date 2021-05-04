3b38239433d56d7f7480fbb349ab8b5465d734b8
viktorholk <viktorholk@gmail.com>
2021-05-04 14:13:34 +0200

    TodoApi

---
+++ H1/Networking/TodoApi/TodoApi/Migrations/20210504121144_migration2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApi.Migrations
+{
+    public partial class migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Secret",
+                table: "TodoItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Secret",
+                table: "TodoItems");
+        }
+    }
+}
