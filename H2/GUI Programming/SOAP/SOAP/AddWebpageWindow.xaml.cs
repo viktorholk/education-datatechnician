5360e93fc75079dad755778749c65e9accdf0b94
viktorholk <viktorholk@gmail.com>
2021-09-01 08:02:27 +0200

    SOAP assignment

---
+++ H2/GUI Programming/SOAP/SOAP/AddWebpageWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SOAP.ServiceReference1;
+namespace SOAP
+{
+    /// <summary>
+    /// Interaction logic for AddWebpageWindow.xaml
+    /// </summary>
+    public partial class AddWebpageWindow : Window
+    {
+        public AddWebpageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddWebpageButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Add new homepage
+            if (NameTextBox.Text != "" && UrlTextBox.Text != "")
+            {
+                // Description can be empty
+
+                // Create the Hjemmeside object
+                Hjemmeside webPage = new Hjemmeside
+                {
+                    Navn = NameTextBox.Text,
+                    Url = UrlTextBox.Text,
+                    Beskrivelse = DescriptionTextBox.Text
+                };
+
+                // add the new hjemmeside to the SOAP service
+                Service1Client client = new Service1Client();
+                client.addHjemmesider(webPage);
+                // Add to the stackpanel
+                // Close the window
+                this.Close();
+            }
+        }
+    }
+}
---
+++ H2/GUI Programming/SOAP/SOAP/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SOAP.ServiceReference1;
+
+namespace SOAP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Hjemmeside> Hjemmesider;
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Hide script errors from webpageBrowser
+            HideScriptErrors(webpageBrowser, true);
+
+            // create the hjemmesider instance
+            Hjemmesider = new List<Hjemmeside>();
+
+            // Add the buttons from SOAP
+            RefreshButtons();
+
+        }
+
+        private void RefreshButtons()
+        {
+            Service1Client SOAP = new Service1Client();
+
+            Hjemmeside[] hjemmesider = SOAP.getHjemmesider();
+            foreach (var hjemmeside in hjemmesider)
+            {
+                // Make sure we only add buttons that doesn't already exists
+                if (!Hjemmesider.Any(i => i.Url == hjemmeside.Url))
+                {
+                    // Create the button
+                    Button button = new Button
+                    {
+                        Tag = hjemmeside.Url,
+                        Content = hjemmeside.Navn
+                    };
+                    // Style and alignment for button
+                    button.Width = 125;
+                    Thickness _margin = button.Margin;
+                    _margin.Top += 5;
+                    button.Margin = _margin;
+
+                    // Set the event
+                    button.Click += (sender, args) => {
+                        string url = button.Tag.ToString();
+                        webpageTextBox.Text = url;
+                        webpageBrowser.Source = new Uri($"https://{url}");
+                    };
+
+                    // Add to stack panel
+                    ScrollStack.Children.Add(button);
+                    // Add to Hjemmesider list so we wont be adding the same button more than once
+                    Hjemmesider.Add(hjemmeside);
+
+                }
+            }
+        }
+        private void AddWebPage_Click(object sender, RoutedEventArgs e)
+        {
+            AddWebpageWindow addWebpageWindow = new AddWebpageWindow();
+            addWebpageWindow.Show();
+
+        }
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshButtons();
+        }
+
+        public void HideScriptErrors(WebBrowser wb, bool hide)
+        {
+            var fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+            var objComWebBrowser = fiComWebBrowser.GetValue(wb);
+            if (objComWebBrowser == null)
+            {
+                wb.Loaded += (o, s) => HideScriptErrors(wb, hide); //In case we are to early
+                return;
+            }
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { hide });
+        }
+    }
+}
