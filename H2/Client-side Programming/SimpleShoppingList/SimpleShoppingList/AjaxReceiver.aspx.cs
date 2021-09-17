cc2564076f1da6ff2753882866db958c03921ebf
viktorholk <viktorholk@gmail.com>
2021-09-17 10:10:55 +0200

    SimpleShoppingList assignment

---
+++ H2/Client-side Programming/SimpleShoppingList/SimpleShoppingList/AjaxReceiver.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+namespace SimpleShoppingList
+{
+    class ItemList
+    {
+        public string ListName;
+        public List<Item> Items;
+
+        public ItemList(string listName, List<Item> items)
+        {
+            this.ListName = listName;
+            this.Items = items;
+        }
+    }
+    class Item
+    {
+        public string Name;
+        public int Price;
+        
+        public Item(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+
+    public partial class AjaxReceiver : System.Web.UI.Page
+    {
+        static readonly List<ItemList> ItemLists = new List<ItemList>()
+        {
+            new ItemList("Frugt og Grønt", new List<Item>(){
+                new Item("Blender", 150),
+                new Item("Drikkedunk", 2),
+                new Item("Sports Brus", 15),
+            }),
+            new ItemList("delikatesse", new List<Item>()
+            {
+                new Item("FIsk", 20),
+                new Item("Stor fisk", 25),
+                new Item("Død laks", 12),
+                new Item("Hugorm", 3000)
+            })
+        };
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true)]
+        public static string GetLists()
+        {
+            return JsonConvert.SerializeObject(ItemLists);
+        }
+    }
+}
---
+++ H2/Client-side Programming/SimpleShoppingList/SimpleShoppingList/Scripts/app.js
@@ -0,0 +1,116 @@
+﻿var currentList = {}
+var serverLists = [];
+
+function createShoppingList() {
+    currentList.name = $("#shoppingListName").val();
+    currentList.items = new Array();
+
+    //Web Service Call
+    showShoppingList();
+
+}
+
+function showShoppingList(id) {
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+
+    $("#newItemName").focus();
+    $("#newItemName").keyup(function (event) {
+        if (event.keyCode == 13) {
+            addItem();
+        }
+    });
+}
+
+function addItem() {
+    console.log(currentList);
+    currentList.items.push({
+        Name: $("#newItemName").val(),
+        Price: 0
+    });
+    console.info(currentList);
+    drawItems();
+    $("#newItemName").val("");
+}
+
+function drawItems() {
+    var $list = $("#shoppingListItems").empty();
+    console.log(currentList);
+    for (var i = 0; i < currentList.items.length; i++) {
+        var currentItem = currentList.items[i];
+        var $li = $("<li>").html(`${currentItem.Name}, <b>${currentItem.Price}</b>,- DK`).attr("id", "item_" + i);
+        var $deleteBtn = $("<button onclick='deleteItem(" + i + ")'>D</button>").appendTo($li);
+        var $checkBtn = $("<button onclick='checkItem(" + i + ")'>C</button>").appendTo($li);
+
+        $li.appendTo($list);
+    }
+}
+
+function deleteItem(index) {
+    currentList.items.splice(index, 1);
+    drawItems();
+}
+
+function checkItem(index) {
+    if ($("#item_" + index).hasClass("checked")) {
+        $("#item_" + index).removeClass("checked");
+    }
+    else {
+        $("#item_" + index).addClass("checked");
+    }
+
+
+    $("#item_" + index).addClass("checked");
+}
+
+function getShoppingListById(id) {
+    console.info(id);
+
+    const list = serverLists[id];
+
+    currentList.name = list.ListName;
+    currentList.items = list.Items;
+
+    showShoppingList();
+    drawItems();
+}
+
+
+
+
+$(document).ready(function () {
+    console.info("ready");
+    $("#shoppingListName").focus();
+    $("#shoppingListName").keyup(function (event) {
+        if (event.keyCode == 13) {
+            createShoppingList();
+        }
+    });
+
+
+    $.ajax({
+        method: "GET",
+        url: "AjaxReceiver.aspx/GetLists",
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        error: (result) => {
+            console.error(result);
+        },
+        success: (result) => {
+            // Get the data and add it to the serverLists variables
+            const data = JSON.parse(result.d);
+            serverLists = data;
+
+            // Add the items as buttons to the page
+            for (let i in data) {
+                const item = data[i];
+                console.log(item);
+
+                $('#listButtons').append(`<Button onclick="getShoppingListById(${i})" >${item.ListName}</Button>`)
+            }
+        } 
+    })
+});
