e46a484ab1f27daefafd036489e2140b52d516ff
viktorholk <holkinator@gmail.com>
2022-10-24 15:38:48 +0200

    LoginApplication

---
+++ H3/Serverside Programming/LoginApplication/LoginApplication/DebugMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LoginApplication
+{
+    public class DebugMiddleware
+    {
+        private RequestDelegate _next;
+
+        public DebugMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var path = context.Request.Path;
+
+            var ipAddress = context.Request.HttpContext.Connection.LocalIpAddress;
+
+            var port = context.Request.HttpContext.Connection.LocalPort;
+
+
+            DateTime dateTime = DateTime.Now;
+
+            using (StreamWriter writer = new StreamWriter("log.txt", append: true))
+            {
+                await writer.WriteAsync($"{dateTime.ToString("g")} - {path.ToString()} - {(ipAddress.ToString() == "::1" ? "localhost" : ipAddress.ToString())}:{port.ToString()}\n");
+            }
+
+            await _next(context);
+
+        }
+    }
+}
+
---
+++ H3/Serverside Programming/LoginApplication/LoginApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/authenticate", async (context) =>
+{
+    string username = context.Request.Query["username"].ToString() ?? "";
+    string password = context.Request.Query["password"].ToString() ?? "";
+
+    if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+    {
+        await context.Response.WriteAsync("Missing username or password parameters");
+        return;
+    }
+
+
+    var availableLogins = new Dictionary<string, string>()
+    {
+        {"sample", "password" },
+        {"admin", "admin" },
+    };
+
+    bool validLogin = false;
+    foreach(var login in availableLogins)
+    {
+        if (login.Key == username && login.Value == password)
+            validLogin = true;
+    }
+
+    if (validLogin)
+        context.Response.Redirect("/content.html");
+    else
+        context.Response.Redirect("/401.html");
+});
+
+app.UseMiddleware<LoginApplication.DebugMiddleware>();
+app.UseStaticFiles();
+app.Run();
+
