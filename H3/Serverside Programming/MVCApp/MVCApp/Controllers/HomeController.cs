8326094d5200d5652a6e81aed231f2a74079d56c
viktorholk <holkinator@gmail.com>
2022-10-31 16:44:56 +0100

    Project

--- H3/Serverside Programming/MVCApp/MVCApp/Controllers/HomeController.cs
+++ H3/Serverside Programming/MVCApp/MVCApp/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace MVCApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private static Calculation? _calculation = null;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -25,14 +27,24 @@ namespace MVCApp.Controllers
 
         public IActionResult Math()
         {
-            Calculation calculation = new(new int[] { 5, 5 }, Operator.Multiplication);
+            Random random = new Random();
+            _calculation = new(new int[] { random.Next(1, 11), random.Next(1, 11) }, (Operator)random.Next(3));
 
-            return View(calculation);
+            return View(_calculation);
         }
 
-        public IActionResult MathValidate([FromForm] Calculation calculation)
+        public IActionResult MathValidate(IFormCollection keyValuePairs)
         {
-            return View();
+            if (_calculation is null)
+                RedirectToAction("Math");
+
+            int answer = Convert.ToInt32(keyValuePairs["Answer"]);
+
+            _calculation.Answer = answer;
+
+            _logger.LogInformation(_calculation.ToString());
+
+            return View(_calculation);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
