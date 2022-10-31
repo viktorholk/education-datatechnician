22917366aef8520a76e86e2cf260447d9d41e9fa
viktorholk <holkinator@gmail.com>
2022-10-31 11:07:53 +0100

    MVCApp

---
+++ H3/Serverside Programming/MVCApp/MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+using System.Diagnostics;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Math()
+        {
+            Calculation calculation = new(new int[] { 5, 5 }, Operator.Multiplication);
+
+            return View(calculation);
+        }
+
+        public IActionResult MathValidate([FromForm] Calculation calculation)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ H3/Serverside Programming/MVCApp/MVCApp/Models/Calculation.cs
@@ -0,0 +1,42 @@
+﻿namespace MVCApp.Models
+{
+    public enum Operator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+    public class Calculation
+    {
+        public int[] Values { get; private set; }
+        public Operator Operator { get; private set; }
+        public int Answer { get; set; }
+
+        public Calculation(int[] values, Operator @operator)
+        {
+            if (values.Length != 2)
+                throw new ArgumentException("Value array needs to be containing 2 values");
+
+            Values = values;
+            Operator = @operator;
+        }
+
+        public int Result()
+        {
+            return Operator switch
+            {
+                Operator.Addition       => Values[0] + Values[1],
+                Operator.Subtraction    => Values[0] - Values[1],
+                Operator.Multiplication => Values[0] * Values[1],
+                Operator.Division       => Values[0] / Values[1],
+                _                       => 0,
+            };
+        }
+
+        public bool Validate()
+        {
+            return Answer == Result();
+        }
+    }
+}
