437c9be084e283f8ff0604e102b71cc9e0e6346c
viktorholk <holkinator@gmail.com>
2022-12-08 09:08:33 +0100

    w

--- H3/Embedded Programming/C Assignments/time.c
+++ H3/Embedded Programming/C Assignments/time.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 
+#define MINUTE_IN_SECONDS 60
+#define HOUR_IN_SECONDS 3600
+#define DAY_IN_SECONDS 86400
+#define WEEK_IN_SECONDS 604800
+#define MONTH_IN_SECONDS 2629743
+#define YEAR_IN_SECONDS 31556926
+
 const char format[] = "%c\nYear: %Y\nMonth: %B\nDay: %A\nWeek Number: %U\nTime: %H:%M:%S\n";
 
 void usingTimeLibrary(){
@@ -19,24 +26,30 @@ void usingTimeLibrary(){
 
 }
 
-
-unsigned const int hour = 3600;
-unsigned const int day = 86400;
-unsigned const int week = 604800;
-unsigned const int month = 2629743;
-unsigned const int year = 31556926;
-
 int main(void){
-  usingTimeLibrary(); 
-
+  //usingTimeLibrary(); 
 
   unsigned long time_n = time(NULL);
-
+  
   printf("%lu\n", time_n);
 
-  printf("%d\n", 1970 + (time_n / year));
+  // Years from 1970
+  int second = time_n % 60;
+  int minute = time_n / MINUTE_IN_SECONDS % 60;
+  int hour = time_n / HOUR_IN_SECONDS % 24;
+  int days = time_n / DAY_IN_SECONDS;
+  int years = days / 365;
+  int year = 1970 + years ;
 
-  printf("%d\n", (time_n / month));
+  //int year = time_n / YEAR_IN_SECONDS;
+  //int month = time_n / MONTH_IN_SECONDS / year;
+
+  //// 1970 started with a Thursday so we add 4
+  //int weekDay = ((time_n / DAY_IN_SECONDS) + 4) % 7;
+
+  printf("Date: %d/%d/%d\n", year,days,hour);
+  printf("%d/%d/%d\n", hour,minute,second);
+  
 
 
 
