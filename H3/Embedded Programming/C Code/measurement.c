a909be28886c1247a96dfa86c37fc76d845cdd16
viktorholk <holkinator@gmail.com>
2022-12-14 11:40:58 +0100

    work?

--- H3/Embedded Programming/C Code/measurement.c
+++ H3/Embedded Programming/C Code/measurement.c
@@ -10,52 +10,23 @@ void printbits(unsigned int v) {
 
 int main(void) {
 
-  //int reading[] = {0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0,
-  //                 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0};
-
-  //unsigned char bytes[4];
-
-  //for (int i = 0; i < 4; i++) {
-
-  //  for (int j = 7; j >= 0; j--) {
-  //    bytes[i] |= (reading[8 * i + j] << j);
-  //  }
-  //}
-
-  //for (int i = 0; i < 4; i++) {
-  //  printf("%x\n", bytes[i]);
-  //}
-
   int bits[] = {0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0,
-                0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0};
+                0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0};
 
   // Define an array to hold the resulting bytes
-  unsigned char bytes[4];
+  unsigned char bytes[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
 
   // Iterate over the bytes in the array
-  for (int i = 0; i < 4; i++) {
-    // Iterate over the bits in the byte
-    for (int j = 7; j >= 0; j--) {
-      // Set the j-th bit of the i-th byte to the value of the (8 * i + j)-th
-      // bit in the array
-      bytes[i] |= (bits[8 * i + j] << j);
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 8; j++) {
+      bytes[i] |= (bits[8 * i + (7 - j)] << j);
     }
   }
 
   // Print the resulting bytes
-  for (int i = 0; i < 4; i++) {
-    printf("%x ", bytes[i]); // Output: 00 d0 00 00
+  for (int i = 0; i < 5; i++) {
+    printf("%d ", bytes[i]);
   }
 
-  // int r[] = {1, 0, 1, 0, 0, 1, 1, 0};
-
-  // unsigned char byte = 0;
-
-  // for (int i = 7; i >= 0; i--) {
-  //   // Set the i-th bit of the byte to the value of the i-th bit in the array
-  //   byte |= (r[i] << (7 - i));
-  // }
-
-  // printf("%d", byte);
   return 0;
 }
