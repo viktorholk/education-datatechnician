ae6f0fedf2d0e9c43b1572297fa90d5175b0987f
viktorholk <holkinator@gmail.com>
2022-12-15 11:22:09 +0100

    cirbuf

--- H3/Embedded Programming/IAR Projects/Cirbuf/cirbuf/dht22.c
+++ H3/Embedded Programming/IAR Projects/Cirbuf/cirbuf/dht22.c
@@ -51,6 +51,7 @@ Experimentaly 0 and 1 timing is not all within time limits - based on one device
 #include <io430.h>
 #include "dht22.h"
 #include <stdio.h>
+#include <stdint.h>
 //DHT22ARRAYSIZE explained above in comment
 #define ARRAYSIZE 43
 #define ARRAYFIRSTBIT 3
@@ -126,34 +127,87 @@ void dht22_read(unsigned int *dht22_array, char *event) {
 // Return 0 on succes
 // Return -1 to indicate invalid timer value or other error from ISR
 // return -2 to indicate parity error
-signed int dht22_convert(unsigned int *dht22_arr, dht22_t *dht22) {
-  // DEVELOP THIS FUNCTION
-  
-  
-  int bit_arr[40];
-  for (int i = 2; i < 43; i++){
-    int result = dht22_arr[i + 1] - dht22_arr[i];
-    
-    int bit = (result < 110) ? 0 : 1;
-    
-    bit_arr[i - 2] = bit;
+signed int dht22_convert(unsigned int *dht22arr, dht22_t *dht22) {
 
-    printf("%d", bit);
+  // Convert to bit array
+  unsigned char bit_arr[40];
+
+  //Print all readings
+  for (int i = 0; i < 43; i++)
+  {
+    printf("%d ", dht22arr[i]);
+  }
+           printf("\n\r");
+  
+  // Scrap the first 3 readings
+  const int offset = 2;
+
+  for (int i = offset; i < 42; i++) {
+    const char difference = dht22arr[i + 1] - dht22arr[i];
+    char bit = (difference < 110) ? 0 : 1;
+
+    bit_arr[i - offset] = bit;
+
+    printf("%-2d %d-%d = %-3d = %d\n\r", i - offset + 1, dht22arr[i + 1],
+           dht22arr[i], difference, bit);
   }
   printf("\n\r");
-  
-  // 40 / 8 = 5
-  // each is an octet
-  
-  for (int i = 0; i < 40; i++){
-    
-    printf("%d",bit_arr[i]);
-    
-  
 
+  for (int i = 0; i < 40; i++) {
+    printf("%d", bit_arr[i]);
+    if ((i + 1) % 8 == 0)
+      printf(" ");
   }
 
-return 0;
+  printf("\n\r");
+  // Define an array to hold the resulting bytes
+  unsigned char bytes[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
+
+  // Iterate over the bytes in the array
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 8; j++) {
+      bytes[i] |= (char)(bit_arr[8 * i + (7 - j)] << j);
+    }
+  }
+
+  // Print the resulting bytes
+  for (int i = 0; i < 5; i++) {
+    printf("%-9d", bytes[i]);
+  }
+  printf("\n\r");
+
+  if (!(bytes[0] + bytes[1] + bytes[2] + bytes[3] == bytes[4]))
+    return -2;
+
+  printf("\n\r");
+
+  // byte[0] = 0000 0010
+  // byte[1] = 1001 0010
+  // byte[0] << 8 = 0010 0000 0000
+  // byte[0] << 8 | bytes[1] = 0010 0000 0000 | 1001 0010 = 0010 1001 0010
+  dht22->humidity = ((bytes[0] << 8) | bytes[1]) / 10;
+
+  // Check if minus flag is present
+  signed int temperature;
+  int negativeTemperatureFlag = 0;
+
+  // If the 8 bit in the temperature byte is present it is a flag that the
+  // temperature is negative
+  if (bytes[2] & 0x80) {
+    negativeTemperatureFlag = 1;
+
+    // Clear flag
+    bytes[2] &= ~0x80;
+  }
+
+  temperature = ((bytes[2] << 8) | bytes[3]) / 10;
+
+  // Convert to negative number if the flag has been set
+  if (negativeTemperatureFlag)
+    temperature = ~(temperature) + 1;
+
+  dht22->temperature = temperature;
+  return 0;
 }
 
 #pragma vector=TIMER2_A1_VECTOR
--- H3/Embedded Programming/IAR Projects/Cirbuf/cirbuf/main.c
+++ H3/Embedded Programming/IAR Projects/Cirbuf/cirbuf/main.c
@@ -24,29 +24,30 @@ cirbuf_str_t cb_in;
 char message[CONSOLE_IN_MES_SIZE];
 
 unsigned int dht22_arr[43];
-dht22_t dht22;
+dht22_t data;
 
 int main(void) {
   
   // Initialize  
-    
+  
   WDTCTL = WDTPW + WDTHOLD;
   ucs_init(1);  // Default MCLK=SMCLK=~ 1 MHz, ACLK=~32K768
   ht_cirbuf_init(&cb_out, con_out_buf, sizeof(con_out_buf));
   ht_cirbuf_init(&cb_in, con_in_buf, sizeof(con_in_buf));
   uart_init(&cb_out, &cb_in, &event[CONSOLE_IN]);  
   ta0_init(1250, &event[TIMER_TA0]); // ta0 clock: 1.000.000 / 8 = 125 Khz
-   
+  
   message[0] = 0x00; // Empty string
   //Initialize events array to 0 events
   for (int i=0; i < EVENTS; i++)
     event[i] = 0;
   
   __bis_SR_register(GIE); 
+  
   //Prepare console nicely :-)
   console_reset();           // Clear and reset terminal
   console_cls();
-
+  
   //event_console_in_init(6);
   
   printf("DHT22 Program\n\r");
@@ -55,20 +56,26 @@ int main(void) {
   
   for (int i = 0; event[DHT22] == 0; i++);
   
-  int status = dht22_convert(dht22_arr, &dht22);
+  // Only pass the 40 readings
+  
+  int status = dht22_convert(dht22_arr, &data);
+  
+  switch (status) {
+  case 0:
+    printf("Humidity: %d\n\r", data.humidity);
+    printf("Temperature: %d\n\r", data.temperature);
+    break;
+  case -2:
+    printf("Parity checksum mismatch!");
+    break;
+  default:
+    printf("Something went wrong");
+    break;
+  }  
   
   while(1);
   
-// Hardwired scheduler - Main program logic  
-   while(1) {
-     if (event[DHT22] > 0){
-        event[DHT22]--;
-        event_dht22_in(event[DHT22], &dht22);
-     }
-    
-   }
 }
-  
-  
-  
-  
+
+
+
